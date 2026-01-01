c21749627cdaffecc454b26926213be1479c275b
mohadese <mohiw.1380@gmail.com>
2026-01-01 17:14:02 +0330

    remove build files from repo

--- build/routers/app.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = require("express");
-const _1_1 = tslib_1.__importDefault(require("./versions/1"));
-const router = (0, express_1.Router)();
-router.use("/api/v1", _1_1.default);
-exports.default = router;
--- build/routes/versions/1/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = require("express");
-const application_routes_1 = tslib_1.__importDefault(require("./application.routes"));
-const router = (0, express_1.Router)();
-router.use("/application", application_routes_1.default);
-exports.default = router;
