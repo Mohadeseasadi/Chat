f40380430de4498d14d3f0c2cd23ac1f901f14d8
mohadese <mohiw.1380@gmail.com>
2026-01-02 18:05:08 +0330

    init: template engin enj

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,31 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
+import path from "path";
 import ChatRoomConnectDB from "./base/database.base";
+import ChatRoomErrorHandling from "./base/error.base";
 import ChatRoomResponse from "./base/response.base";
 import ChatRoomRoutes from "./base/routes.base";
 import router from "./routers/app.routes";
-import ChatRoomErrorHandling from "./base/error.base";
+import viewRouter from "./routers/view.routes";
 
 dotenv.config();
 const app = express();
 
-// test git user
-
+// config
 app.use(express.json({ limit: "10mb" }));
 app.use(json());
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+// use template engin
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", viewRouter);
+
 // connect to database
 ChatRoomConnectDB();
 // customize Response
---
+++ src/routers/view.routes.ts
@@ -0,0 +1,35 @@
+import { Request, Response, Router } from "express";
+
+const viewRouter = Router();
+
+viewRouter.get("/login", (req: Request, res: Response) => {
+  res.render("login", {
+    pageTitle: "Login",
+    headerText: "Join to App",
+    buttonText: "Login",
+    formId: "loginForm",
+    bottomText: "Don't have an account?",
+    bottomLink: "/signup",
+    bottomLinkText: "Sign up",
+    formJs: "login.js",
+  });
+});
+
+viewRouter.get("/signup", (req: Request, res: Response) => {
+  res.render("login", {
+    pageTitle: "Sign Up",
+    headerText: "Create Account",
+    buttonText: "Sign Up",
+    formId: "signupForm",
+    bottomText: "Already have an account?",
+    bottomLink: "/login",
+    bottomLinkText: "Login",
+    formJs: "signup.js",
+  });
+});
+
+viewRouter.get("/chat", (req: Request, res: Response) => {
+  res.render("chat");
+});
+
+export default viewRouter;
